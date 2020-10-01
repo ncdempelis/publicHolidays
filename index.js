e5f99c21466ee6969658412c343c64bb52faa4b6
Nick Dempelis <dempelis@gmail.com>
2020-10-01 14:19:21 +0300

    Trying to figure out how to store the defaults

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 //(function () {
 	'use strict';
 
-	const REGIONS =  {'GR' : {currency: 'EURO', flags: {includeSaturdays: false, includeCurrency: false, includeSchool: false}},
-			  'UK' : {currency: 'GBP' , flags: {includeSaturdays: false, includeCurrency: false, includeSchool: false}}
+	const REGIONS =  {
+		'GR' : {
+			currency: 'EURO', 
+			flags: {includeSundays: true, includeSaturdays: false, includeCurrency: false, includeSchool: false}, 
+			fixed: { // 0 => Ιανουάριος ...
+				0: [1,6],
+				2: [25],
+				4: [1],
+				7: [15],
+				11: [25,26] 
+			}, 
+			movable:{//relative to orthodox easter
+				cleanMonday: -48,
+				greatFriday: -2,
+				greatSaturday: -1,
+				easterMonday: 1,
+				holySpirit:50
+			}
+		},
+		'UK' : {
+			currency: 'GBP', 
+			flags: {includeSaturdays: false, includeCurrency: false, includeSchool: false}}
 	};
+
+	for (const [key, value] of Object.entries(REGIONS)){
+		Object.freeze(key.fixed);
+		Object.freeze(key.movable);
+		Object.freeze(key.flags);
+		Object.seal(key);
+	}
+	// Object.freeze(REGIONS.GR.fixed);
+	// Object.freeze(REGIONS.GR.movable);
+	// Object.freeze(REGIONS.GR.flags);
+	// Object.freeze(REGIONS.GR);
+
+	
 	
 	const isSunday = aDate => aDate.getDay() === 0 ;
 	const isSaturday = aDate => aDate.getDay() === 6;
@@ -20,15 +53,14 @@
 
 
 
-		return false; }
-//	return {REGIONS};
-//})();
+		return false; 
+	}
 
 	REGIONS['Italy'] = { a:1, b:2, c:3};
-
-	console.dir(REGIONS);
-	console.dir(REGIONS.GR);
-	
+	REGIONS.GR.currency='USD';
+	// console.dir(REGIONS);
+	 console.table(REGIONS.GR);
+	console.table(Object.entries(REGIONS));
 	isPublicHoliday(new Date(2020,8,30),'GR');
 	isPublicHoliday(new Date(2020,8,30),'GR',{includeSaturdays: true});
 	isPublicHoliday(new Date(2020,8,30),'KR');
